5f219ccef3d219acb7b2a2f36aad4f0611b3cb0f
gusawski <gustav@mohr.dk>
2021-11-25 15:05:10 +0100

    Removed dev js files

--- resources/public/cljs-out/dev/cljs/user/beregningerD4366BA.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.10.773 {}
-
-//# sourceMappingURL=beregningerD4366BA.js.map
--- resources/public/cljs-out/dev/website/core_test.cljs
+++
@@ -1,5 +0,0 @@
-(ns website.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     ))
-
